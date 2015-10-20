a700ec3b8e4b2f7dbf212cc4dea62b474d9a7981
Guest User <student@epicodus.com>
2015-10-20 09:53:13 -0700

    Allow User to edit their comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to root_path, notice: 'Comment was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :users
+  resources :sessions
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   root 'posts#index'
 
-  resources :users
-  resources :sessions
 end
